f46d23f3c2f8cb8bb22d02fc70887c49f5f6cffd
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-05-23 15:57:14 +0700

    Итерация4 Прописаны методы классов с заглушками

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 
 
 
-class Tourist:
-    pass
-
 class TravelAgency:
-    pass
+    __count_tips = 0
+    __map_places = ["Istanbul", "Ankara", "Sochi", "Gelendzhik", "Denpasar", "Paris"]
+    __degree_of_comfort = [1, 2, 3, 4, 5]
+
+    def set_tips_count(self, count):
+        pass
+
+    def get_tip(self):
+        pass
+
+    def get_tips(self):
+        pass
+
+
+class Tourist:
+    tips = []
+
+    def set_variants(self, tips):
+        pass
+
+    def get_choice(self):
+        pass
 
 def main():
     travel_agency = TravelAgency()
@@ -17,6 +35,7 @@ def main():
     tourist.set_variants(travel_agency_tips)
 
     print(tourist.get_choice())
+
 
 
 if __name__ == '__main__':
