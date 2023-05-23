bfd5583c1979377525356fa03723b659da3327c4
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-05-23 15:46:56 +0700

    Итерация3 Логика основной функции прописана, созданы классы для сущностей с заглушкой

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 
 
 
-def main():
+class Tourist:
     pass
+
+class TravelAgency:
+    pass
+
+def main():
+    travel_agency = TravelAgency()
+    tourist = Tourist()
+
+    travel_agency.set_tips_count(10)
+    travel_agency_tips = travel_agency.get_tips()
+
+    tourist.set_variants(travel_agency_tips)
+
+    print(tourist.get_choice())
+
 
 if __name__ == '__main__':
     main()
